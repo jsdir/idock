09de2f8fcc63cb1d82986d60d6418567062146f8
Jacky Lee <JackyLeeHongJian@Gmail.com>
2013-06-24 14:55:25 +0800

    Revised utility.hpp

--- src/utility.hpp
+++ src/utility.hpp
@@ -7,24 +7,19 @@
 #include <cmath>
 using namespace std;
 
-/// Returns the flattened 1D index of a 2D index (i, j) where j is the lowest dimension.
-inline size_t mr(const size_t i, const size_t j)
+/// Returns the flattened 1D index of a triangular 2D index (x, y) where x is the lowest dimension.
+inline size_t mr(const size_t x, const size_t y)
 {
-	return (j*(j+1)>>1) + i;
+	return (y*(y+1)>>1) + x;
 }
 
-/// Returns the flattened 1D index of a 2D index (i, j) where either i or j is the lowest dimension.
-inline size_t mp(const size_t i, const size_t j)
+/// Returns the flattened 1D index of a triangular 2D index (x, y) where either x or y is the lowest dimension.
+inline size_t mp(const size_t x, const size_t y)
 {
-	return i <= j ? mr(i, j) : mr(j, i);
+	return x <= y ? mr(x, y) : mr(y, x);
 }
 
-/// Returns true is the vector is (0, 0, 0).
-inline bool zero(const array<float, 3>& a)
-{
-	return a[0] < 1e-5f && a[1] < 1e-5f && a[2] < 1e-5f;
-}
-
+/// Returns an array containing 3 given floats.
 inline array<float, 3> make_array(const float d0, const float d1, const float d2)
 {
 	array<float, 3> a;
@@ -34,87 +29,7 @@ inline array<float, 3> make_array(const float d0, const float d1, const float d2
 	return a;
 }
 
-/// Returns the result of pairwise multiplication of the current vector and the given vector.
-inline array<float, 3> operator*(const array<float, 3>& a, const array<size_t, 3>& b)
-{
-	return make_array
-	(
-		a[0] * b[0],
-		a[1] * b[1],
-		a[2] * b[2]
-	);
-}
-
-/// Returns the result of pairwise addition of the current vector and the given vector.
-inline array<float, 3> operator+(const array<float, 3>& a, const array<float, 3>& b)
-{
-	return make_array
-	(
-		a[0] + b[0],
-		a[1] + b[1],
-		a[2] + b[2]
-	);
-}
-
-/// Returns the result of pairwise subtraction of the current vector and the given vector.
-inline array<float, 3> operator-(const array<float, 3>& a, const array<float, 3>& b)
-{
-	return make_array
-	(
-		a[0] - b[0],
-		a[1] - b[1],
-		a[2] - b[2]
-	);
-}
-
-/// Pairwise add a given vector to the current vector.
-inline void operator+=(array<float, 3>& a, const array<float, 3>& b)
-{
-	a[0] += b[0];
-	a[1] += b[1];
-	a[2] += b[2];
-}
-
-/// Pairwise subtract a given vector from the current vector.
-inline void operator-=(array<float, 3>& a, const array<float, 3>& b)
-{
-	a[0] -= b[0];
-	a[1] -= b[1];
-	a[2] -= b[2];
-}
-
 const array<float, 3> zero3 = make_array(0.0f, 0.0f, 0.0f); ///< Constant vector with all the 3 elements of zero.
-
-/// Pairwise multiply a constant to the current vector.
-inline array<float, 3> operator*(const float s, const array<float, 3>& v)
-{
-	return make_array
-	(
-		s * v[0],
-		s * v[1],
-		s * v[2]
-	);
-}
-
-/// Returns the cross product of two vectors.
-inline array<float, 3> cross_product(const array<float, 3>& a, const array<float, 3>& b)
-{
-	return make_array
-	(
-		a[1]*b[2] - a[2]*b[1],
-		a[2]*b[0] - a[0]*b[2],
-		a[0]*b[1] - a[1]*b[0]
-	);
-}
-
-/// Returns the square distance between two vectors.
-inline float distance_sqr(const array<float, 3>& a, const array<float, 3>& b)
-{
-	const float d0 = a[0] - b[0];
-	const float d1 = a[1] - b[1];
-	const float d2 = a[2] - b[2];
-	return d0 * d0 + d1 * d1 + d2 * d2;
-}
 
 /// Returns the square norm.
 inline float norm_sqr(const array<float, 3>& a)
@@ -146,6 +61,87 @@ inline array<float, 3> normalize(const array<float, 3>& a)
 	);
 }
 
+/// Returns pairwise addition of 2 given arrays.
+inline array<float, 3> operator+(const array<float, 3>& a, const array<float, 3>& b)
+{
+	return make_array
+	(
+		a[0] + b[0],
+		a[1] + b[1],
+		a[2] + b[2]
+	);
+}
+
+/// Returns pairwise subtraction of 2 given arrays.
+inline array<float, 3> operator-(const array<float, 3>& a, const array<float, 3>& b)
+{
+	return make_array
+	(
+		a[0] - b[0],
+		a[1] - b[1],
+		a[2] - b[2]
+	);
+}
+
+/// Pairwise add a given vector to the current vector.
+inline void operator+=(array<float, 3>& a, const array<float, 3>& b)
+{
+	a[0] += b[0];
+	a[1] += b[1];
+	a[2] += b[2];
+}
+
+/// Pairwise subtract a given vector from the current vector.
+inline void operator-=(array<float, 3>& a, const array<float, 3>& b)
+{
+	a[0] -= b[0];
+	a[1] -= b[1];
+	a[2] -= b[2];
+}
+
+/// Pairwise multiply a constant to an array.
+inline array<float, 3> operator*(const float s, const array<float, 3>& a)
+{
+	return make_array
+	(
+		s * a[0],
+		s * a[1],
+		s * a[2]
+	);
+}
+
+/// Returns pairwise multiplication of 2 given arrays.
+inline array<float, 3> operator*(const array<float, 3>& a, const array<size_t, 3>& b)
+{
+	return make_array
+	(
+		a[0] * b[0],
+		a[1] * b[1],
+		a[2] * b[2]
+	);
+}
+
+/// Returns the cross product of two vectors.
+inline array<float, 3> cross_product(const array<float, 3>& a, const array<float, 3>& b)
+{
+	return make_array
+	(
+		a[1]*b[2] - a[2]*b[1],
+		a[2]*b[0] - a[0]*b[2],
+		a[0]*b[1] - a[1]*b[0]
+	);
+}
+
+/// Returns the square distance between two arrays.
+inline float distance_sqr(const array<float, 3>& a, const array<float, 3>& b)
+{
+	const float d0 = a[0] - b[0];
+	const float d1 = a[1] - b[1];
+	const float d2 = a[2] - b[2];
+	return d0 * d0 + d1 * d1 + d2 * d2;
+}
+
+/// Returns an array containing 4 given floats.
 inline array<float, 4> make_array(const float d0, const float d1, const float d2, const float d3)
 {
 	array<float, 4> a;
@@ -190,11 +186,13 @@ inline array<float, 4> normalize(const array<float, 4>& a)
 /// Constructs a quaternion by a normalized axis and a rotation angle.
 inline array<float, 4> vec4_to_qtn4(const array<float, 3>& axis, const float angle)
 {
-//	assert(axis.normalized());
-	const float s = sin(angle * 0.5f);
+//	assert(normalized(axis));
+	const float h = angle * 0.5f;
+	const float s = sin(h);
+	const float c = cos(h);
 	return make_array
 	(
-		cos(angle * 0.5f),
+		c,
 		s * axis[0],
 		s * axis[1],
 		s * axis[2]
@@ -204,54 +202,80 @@ inline array<float, 4> vec4_to_qtn4(const array<float, 3>& axis, const float ang
 /// Constructs a quaternion by a rotation vector.
 inline array<float, 4> vec3_to_qtn4(const array<float, 3>& rotation)
 {
-	if (zero(rotation))
+	if (norm_sqr(rotation) < 1e-5f)
 	{
 		return make_array(1.0f, 0.0f, 0.0f, 0.0f);
 	}
 	else
 	{
-		const float angle = norm(rotation);
-		const array<float, 3> axis = (1.0f / angle) * rotation;
-		return vec4_to_qtn4(axis, angle);
+		const float h = norm(rotation) * 0.5;
+		const array<float, 3> axis = (0.5f / h) * rotation;
+//		assert(normalized(axis));
+		const float s = sin(h);
+		const float c = cos(h);
+		return make_array
+		(
+			c,
+			s * axis[0],
+			s * axis[1],
+			s * axis[2]
+		);
 	}
 }
 
 /// Returns the product of two quaternions.
-inline array<float, 4> qtn4_mul_qtn4(const array<float, 4>& q1, const array<float, 4>& q2)
+inline array<float, 4> qtn4_mul_qtn4(const array<float, 4>& a, const array<float, 4>& b)
 {
+//	assert(normalized(a));
+//	assert(normalized(b));
     return make_array
 	(
-		q1[0] * q2[0] - q1[1] * q2[1] - q1[2] * q2[2] - q1[3] * q2[3],
-		q1[0] * q2[1] + q1[1] * q2[0] + q1[2] * q2[3] - q1[3] * q2[2],
-		q1[0] * q2[2] - q1[1] * q2[3] + q1[2] * q2[0] + q1[3] * q2[1],
-		q1[0] * q2[3] + q1[1] * q2[2] - q1[2] * q2[1] + q1[3] * q2[0]
+		a[0] * b[0] - a[1] * b[1] - a[2] * b[2] - a[3] * b[3],
+		a[0] * b[1] + a[1] * b[0] + a[2] * b[3] - a[3] * b[2],
+		a[0] * b[2] - a[1] * b[3] + a[2] * b[0] + a[3] * b[1],
+		a[0] * b[3] + a[1] * b[2] - a[2] * b[1] + a[3] * b[0]
 	);
 }
 
-/// Transforms the current quaternion into a 3x3 transformation matrix, e.g. quaternion(1, 0, 0, 0) => identity matrix.
-inline array<float, 9> qtn4_to_mat3(const array<float, 4>& q)
+/// Returns an array containing 9 given floats.
+inline array<float, 9> make_array(const float d0, const float d1, const float d2, const float d3, const float d4, const float d5, const float d6, const float d7, const float d8)
 {
-//	assert(is_normalized());
-	const float aa = q[0]*q[0];
-	const float ab = q[0]*q[1];
-	const float ac = q[0]*q[2];
-	const float ad = q[0]*q[3];
-	const float bb = q[1]*q[1];
-	const float bc = q[1]*q[2];
-	const float bd = q[1]*q[3];
-	const float cc = q[2]*q[2];
-	const float cd = q[2]*q[3];
-	const float dd = q[3]*q[3];
+	array<float, 9> a;
+	a[0] = d0;
+	a[1] = d1;
+	a[2] = d2;
+	a[3] = d3;
+	a[4] = d4;
+	a[5] = d5;
+	a[6] = d6;
+	a[7] = d7;
+	a[8] = d8;
+	return a;
+}
+
+/// Transforms the current quaternion into a 3x3 transformation matrix, e.g. quaternion(1, 0, 0, 0) => identity matrix.
+inline array<float, 9> qtn4_to_mat3(const array<float, 4>& a)
+{
+//	assert(normalized(a));
+	const float ww = a[0]*a[0];
+	const float wx = a[0]*a[1];
+	const float wy = a[0]*a[2];
+	const float wz = a[0]*a[3];
+	const float xx = a[1]*a[1];
+	const float xy = a[1]*a[2];
+	const float xz = a[1]*a[3];
+	const float yy = a[2]*a[2];
+	const float yz = a[2]*a[3];
+	const float zz = a[3]*a[3];
 
 	// http://www.boost.org/doc/libs/1_46_1/libs/math/quaternion/TQE.pdf
 	// http://en.wikipedia.org/wiki/Quaternions_and_spatial_rotation
-	const array<float, 9> r =
-	{
-		aa+bb-cc-dd, 2*(-ad+bc), 2*(ac+bd),
-		2*(ad+bc), aa-bb+cc-dd, 2*(-ab+cd),
-		2*(-ac+bd), 2*(ab+cd), aa-bb-cc+dd
-	};
-	return r;
+	return make_array
+	(
+		ww+xx-yy-zz, 2*(-wz+xy), 2*(wy+xz),
+		2*(wz+xy), ww-xx+yy-zz, 2*(-wx+yz),
+		2*(-wy+xz), 2*(wx+yz), ww-xx-yy+zz
+	);
 }
 
 /// Transforms a vector by a 3x3 matrix.
