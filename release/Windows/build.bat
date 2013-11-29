a5e9cfeb5b07219fc17872e03a43883a17af1a17
Jacky Lee <JackyLeeHongJian@Gmail.com>
2013-11-30 00:15:11 +0800

    Added custom nvcc build in vcxproj

--- release/Windows/build.bat
+++ release/Windows/build.bat
@@ -1,2 +1,1 @@
 msbuild.exe idock.vcxproj /t:Rebuild /p:Configuration=Release;Platform=x64
-"%CUDA_PATH%\bin\nvcc" -arch=compute_11 -fatbin -ccbin "%VS110COMNTOOLS%\..\..\VC\bin\amd64" -o ..\..\bin\idock.fatbin ..\..\src\idock.cu
