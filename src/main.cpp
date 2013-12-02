0e5aaaa373037c0d0b1a99ba97498a22f6242be6
Jacky Lee <JackyLeeHongJian@Gmail.com>
2013-12-02 10:10:46 +0800

    Posted ligand to io service pool for concurrent execution

--- src/main.cpp
+++ src/main.cpp
@@ -433,122 +433,126 @@ int main(int argc, char* argv[])
 		// Wait until a device is ready for execution.
 		const int dev = idle.safe_pop_back();
 
-		// Push the context of the chosen device.
-		checkCudaErrors(cuCtxPushCurrent(contexts[dev]));
-
-		// Find atom types that are presented in the current ligand but are not yet copied to device memory.
-		xs.clear();
-		for (const atom& a : lig.atoms)
+		// Post the ligand from main thread to io service pool.
+		io.post(bind<void>([&,dev](ligand& lig)
 		{
-			const size_t t = a.xs;
-			if (find(xst[dev].cbegin(), xst[dev].cend(), t) == xst[dev].cend())
+			// Push the context of the chosen device.
+			checkCudaErrors(cuCtxPushCurrent(contexts[dev]));
+
+			// Find atom types that are presented in the current ligand but are not yet copied to device memory.
+			vector<size_t> xs;
+			for (const atom& a : lig.atoms)
 			{
-				xst[dev].push_back(t);
-				xs.push_back(t);
-			}
-		}
-
-		// Copy grid maps from host memory to device memory if necessary.
-		if (xs.size())
-		{
-			const size_t map_bytes = sizeof(float) * rec.num_probes_product;
-			for (const auto t : xs)
-			{
-				CUdeviceptr mapd;
-				checkCudaErrors(cuMemAlloc(&mapd, map_bytes));
-				checkCudaErrors(cuMemcpyHtoD(mapd, rec.maps[t].data(), map_bytes));
-				checkCudaErrors(cuMemcpyHtoD(mpsv[dev] + sizeof(mapd) * t, &mapd, sizeof(mapd)));
-			}
-		}
-
-		// Reallocate ligh and ligd should the current ligand elements exceed the default size.
-		const size_t this_lig_elems = lig.get_lig_elems();
-		if (this_lig_elems > lig_elems[dev])
-		{
-			checkCudaErrors(cuMemFreeHost(ligh[dev]));
-			lig_elems[dev] = this_lig_elems;
-			checkCudaErrors(cuMemHostAlloc((void**)&ligh[dev], sizeof(int) * lig_elems[dev], CU_MEMHOSTALLOC_DEVICEMAP));
-			checkCudaErrors(cuMemHostGetDevicePointer(&ligd[dev], ligh[dev], 0));
-			checkCudaErrors(cuMemcpyHtoD(ligv[dev], &ligd[dev], sizeof(ligv[dev])));
-		}
-
-		// Encode the current ligand.
-		lig.encode(ligh[dev]);
-
-		// Compute the number of shared memory bytes.
-		const size_t lig_bytes = sizeof(int) * lig_elems[dev];
-
-		// Reallocate slnd should the current solution elements exceed the default size.
-		const size_t this_sln_elems = lig.get_sln_elems();
-		if (this_sln_elems > sln_elems[dev])
-		{
-			checkCudaErrors(cuMemFree(slnd[dev]));
-			sln_elems[dev] = this_sln_elems;
-			checkCudaErrors(cuMemAlloc(&slnd[dev], sizeof(float) * sln_elems[dev] * num_mc_tasks));
-			checkCudaErrors(cuMemcpyHtoD(slnv[dev], &slnd[dev], sizeof(slnv[dev])));
-		}
-
-		// Clear the solution buffer.
-		checkCudaErrors(cuMemsetD32Async(slnd[dev], 0, sln_elems[dev] * num_mc_tasks, streams[dev]));
-
-		// Launch kernel.
-		void* params[] = { &lig.nv, &lig.nf, &lig.na, &lig.np };
-		checkCudaErrors(cuLaunchKernel(functions[dev], (num_mc_tasks - 1) / 32 + 1, 1, 1, 32, 1, 1, lig_bytes, streams[dev], params, NULL));
-
-		// Reallocate cnfh should the current conformation elements exceed the default size.
-		const size_t this_cnf_elems = lig.get_cnf_elems();
-		if (this_cnf_elems > cnf_elems[dev])
-		{
-			checkCudaErrors(cuMemFreeHost(cnfh[dev]));
-			cnf_elems[dev] = this_cnf_elems;
-			checkCudaErrors(cuMemHostAlloc((void**)&cnfh[dev], sizeof(float) * cnf_elems[dev] * num_mc_tasks, 0));
-		}
-
-		// Copy conformations from device memory to host memory.
-		checkCudaErrors(cuMemcpyDtoHAsync(cnfh[dev], slnd[dev], sizeof(float) * cnf_elems[dev] * num_mc_tasks, streams[dev]));
-
-		// Add a callback to the compute stream.
-		checkCudaErrors(cuStreamAddCallback(streams[dev], []CUDA_CB (CUstream stream, CUresult error, void* data)
-		{
-			checkCudaErrors(error);
-			const shared_ptr<callback_data<int>> cbd(reinterpret_cast<callback_data<int>*>(data));
-			cbd->io.post([=]()
-			{
-				const auto& output_folder_path = cbd->output_folder_path;
-				const auto  max_conformations = cbd->max_conformations;
-				const auto  num_mc_tasks = cbd->num_mc_tasks;
-				const auto& rec = cbd->rec;
-				const auto& f = cbd->f;
-				const auto  dev = cbd->dev;
-				const auto& cnfh = cbd->cnfh;
-				auto& lig = cbd->lig;
-				auto& safe_print = cbd->safe_print;
-				auto& log = cbd->log;
-				auto& idle = cbd->idle;
-
-				// Write conformations.
-				lig.write(cnfh[dev], output_folder_path, max_conformations, num_mc_tasks, rec, f);
-
-				// Output and save ligand stem and predicted affinities.
-				safe_print([&]()
+				const size_t t = a.xs;
+				if (find(xst[dev].cbegin(), xst[dev].cend(), t) == xst[dev].cend())
 				{
-					string stem = lig.filename.stem().string();
-					cout << setw(8) << log.size() + 1 << setw(14) << stem << setw(2) << dev << ' ';
-					for_each(lig.affinities.cbegin(), lig.affinities.cbegin() + min<size_t>(lig.affinities.size(), 9), [](const float a)
+					xst[dev].push_back(t);
+					xs.push_back(t);
+				}
+			}
+
+			// Copy grid maps from host memory to device memory if necessary.
+			if (xs.size())
+			{
+				const size_t map_bytes = sizeof(float) * rec.num_probes_product;
+				for (const auto t : xs)
+				{
+					CUdeviceptr mapd;
+					checkCudaErrors(cuMemAlloc(&mapd, map_bytes));
+					checkCudaErrors(cuMemcpyHtoD(mapd, rec.maps[t].data(), map_bytes));
+					checkCudaErrors(cuMemcpyHtoD(mpsv[dev] + sizeof(mapd) * t, &mapd, sizeof(mapd)));
+				}
+			}
+
+			// Reallocate ligh and ligd should the current ligand elements exceed the default size.
+			const size_t this_lig_elems = lig.get_lig_elems();
+			if (this_lig_elems > lig_elems[dev])
+			{
+				checkCudaErrors(cuMemFreeHost(ligh[dev]));
+				lig_elems[dev] = this_lig_elems;
+				checkCudaErrors(cuMemHostAlloc((void**)&ligh[dev], sizeof(int) * lig_elems[dev], CU_MEMHOSTALLOC_DEVICEMAP));
+				checkCudaErrors(cuMemHostGetDevicePointer(&ligd[dev], ligh[dev], 0));
+				checkCudaErrors(cuMemcpyHtoD(ligv[dev], &ligd[dev], sizeof(ligv[dev])));
+			}
+
+			// Encode the current ligand.
+			lig.encode(ligh[dev]);
+
+			// Compute the number of shared memory bytes.
+			const size_t lig_bytes = sizeof(int) * lig_elems[dev];
+
+			// Reallocate slnd should the current solution elements exceed the default size.
+			const size_t this_sln_elems = lig.get_sln_elems();
+			if (this_sln_elems > sln_elems[dev])
+			{
+				checkCudaErrors(cuMemFree(slnd[dev]));
+				sln_elems[dev] = this_sln_elems;
+				checkCudaErrors(cuMemAlloc(&slnd[dev], sizeof(float) * sln_elems[dev] * num_mc_tasks));
+				checkCudaErrors(cuMemcpyHtoD(slnv[dev], &slnd[dev], sizeof(slnv[dev])));
+			}
+
+			// Clear the solution buffer.
+			checkCudaErrors(cuMemsetD32Async(slnd[dev], 0, sln_elems[dev] * num_mc_tasks, streams[dev]));
+
+			// Launch kernel.
+			void* params[] = { &lig.nv, &lig.nf, &lig.na, &lig.np };
+			checkCudaErrors(cuLaunchKernel(functions[dev], (num_mc_tasks - 1) / 32 + 1, 1, 1, 32, 1, 1, lig_bytes, streams[dev], params, NULL));
+
+			// Reallocate cnfh should the current conformation elements exceed the default size.
+			const size_t this_cnf_elems = lig.get_cnf_elems();
+			if (this_cnf_elems > cnf_elems[dev])
+			{
+				checkCudaErrors(cuMemFreeHost(cnfh[dev]));
+				cnf_elems[dev] = this_cnf_elems;
+				checkCudaErrors(cuMemHostAlloc((void**)&cnfh[dev], sizeof(float) * cnf_elems[dev] * num_mc_tasks, 0));
+			}
+
+			// Copy conformations from device memory to host memory.
+			checkCudaErrors(cuMemcpyDtoHAsync(cnfh[dev], slnd[dev], sizeof(float) * cnf_elems[dev] * num_mc_tasks, streams[dev]));
+
+			// Add a callback to the compute stream.
+			checkCudaErrors(cuStreamAddCallback(streams[dev], []CUDA_CB (CUstream stream, CUresult error, void* data)
+			{
+				checkCudaErrors(error);
+				const shared_ptr<callback_data<int>> cbd(reinterpret_cast<callback_data<int>*>(data));
+				cbd->io.post([=]()
+				{
+					const auto& output_folder_path = cbd->output_folder_path;
+					const auto  max_conformations = cbd->max_conformations;
+					const auto  num_mc_tasks = cbd->num_mc_tasks;
+					const auto& rec = cbd->rec;
+					const auto& f = cbd->f;
+					const auto  dev = cbd->dev;
+					const auto& cnfh = cbd->cnfh;
+					auto& lig = cbd->lig;
+					auto& safe_print = cbd->safe_print;
+					auto& log = cbd->log;
+					auto& idle = cbd->idle;
+
+					// Write conformations.
+					lig.write(cnfh[dev], output_folder_path, max_conformations, num_mc_tasks, rec, f);
+
+					// Output and save ligand stem and predicted affinities.
+					safe_print([&]()
 					{
-						cout << setw(6) << a;
+						string stem = lig.filename.stem().string();
+						cout << setw(8) << log.size() + 1 << setw(14) << stem << setw(2) << dev << ' ';
+						for_each(lig.affinities.cbegin(), lig.affinities.cbegin() + min<size_t>(lig.affinities.size(), 9), [](const float a)
+						{
+							cout << setw(6) << a;
+						});
+						cout << endl;
+						log.push_back(new log_record(move(stem), move(lig.affinities)));
 					});
-					cout << endl;
-					log.push_back(new log_record(move(stem), move(lig.affinities)));
+
+					// Signal the main thread to post another task.
+					idle.safe_push_back(dev);
 				});
+			}, new callback_data<int>(io, output_folder_path, max_conformations, num_mc_tasks, rec, f, dev, cnfh, move(lig), safe_print, log, idle), 0));
 
-				// Signal the main thread to post another task.
-				idle.safe_push_back(dev);
-			});
-		}, new callback_data<int>(io, output_folder_path, max_conformations, num_mc_tasks, rec, f, dev, cnfh, move(lig), safe_print, log, idle), 0));
-
-		// Pop the context after use.
-		checkCudaErrors(cuCtxPopCurrent(NULL));
+			// Pop the context after use.
+			checkCudaErrors(cuCtxPopCurrent(NULL));
+		}, move(lig)));
 	}
 
 	// Synchronize contexts.
