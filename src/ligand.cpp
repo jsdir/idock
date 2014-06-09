cfb586e379eeb5f09daa0eab8bb8919e20eba20d
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-06-09 22:18:35 +0800

    Added REMARK numbers 921 to 925

--- src/ligand.cpp
+++ src/ligand.cpp
@@ -547,11 +547,11 @@ void ligand::write_models(const path& output_ligand_path, const ptr_vector<resul
 		const double rf = f(x);
 
 		ofs << "MODEL     " << setw(4) << (k + 1) << '\n' << setprecision(2)
-			<< "REMARK       NORMALIZED FREE ENERGY PREDICTED BY IDOCK:" << setw(8) << r.e_nd    << " KCAL/MOL\n"
-			<< "REMARK            TOTAL FREE ENERGY PREDICTED BY IDOCK:" << setw(8) << r.e       << " KCAL/MOL\n"
-			<< "REMARK     INTER-LIGAND FREE ENERGY PREDICTED BY IDOCK:" << setw(8) << r.f       << " KCAL/MOL\n"
-			<< "REMARK     INTRA-LIGAND FREE ENERGY PREDICTED BY IDOCK:" << setw(8) << (r.e - r.f) << " KCAL/MOL\n"
-			<< "REMARK    RF-SCORE BINDING AFFINITY PREDICTED BY IDOCK:" << setw(8) << rf        << " PKD\n" << setprecision(3);
+			<< "REMARK 921   NORMALIZED FREE ENERGY PREDICTED BY IDOCK:" << setw(8) << r.e_nd    << " KCAL/MOL\n"
+			<< "REMARK 922        TOTAL FREE ENERGY PREDICTED BY IDOCK:" << setw(8) << r.e       << " KCAL/MOL\n"
+			<< "REMARK 923 INTER-LIGAND FREE ENERGY PREDICTED BY IDOCK:" << setw(8) << r.f       << " KCAL/MOL\n"
+			<< "REMARK 924 INTRA-LIGAND FREE ENERGY PREDICTED BY IDOCK:" << setw(8) << (r.e - r.f) << " KCAL/MOL\n"
+			<< "REMARK 925RF-SCORE BINDING AFFINITY PREDICTED BY IDOCK:" << setw(8) << rf        << " PKD\n" << setprecision(3);
 
 		for (size_t j = 0, heavy_atom = 0, hydrogen = 0; j < num_lines; ++j)
 		{
