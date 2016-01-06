898ff0b3e91169e8d0248a681bdc688af9fce319
Jacky Lee <JackyLeeHongJian@Gmail.com>
2016-01-06 14:03:50 +0800

    Upgraded PDBbind refined set from v2014 to v2015

--- src/random_forest.hpp
+++ src/random_forest.hpp
@@ -38,7 +38,7 @@ public:
 	//! Clears node samples to save memory.
 	void clear();
 private:
-	static const size_t ns = 3444; //!< Number of training samples.
+	static const size_t ns = 3704; //!< Number of training samples.
 	static const array<array<double, nv>, ns> x; //!< Variables of training samples.
 	static const array<double, ns> y; //!< Measured binding affinities of training samples.
 };
